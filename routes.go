bfe7e5af8f280336755537fef335036dc8187de1
Trevor Sawler <trevor.sawler@gmail.com>
2022-01-03 12:40:59 -0400

    comments

--- routes.go
+++ routes.go
@@ -16,14 +16,19 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func routes() *chi.Mux {
+	// create the router
 	mux := chi.NewRouter()
+
+	// this just prints the full path of all assets to the console
 	fs.WalkDir(assets, ".", func(path string, d fs.DirEntry, err error) error {
 		fmt.Println(path)
 		return nil
 	})
 
+	// a sample handler for the home page
 	mux.HandleFunc("/", Home)
 
+	// handle serving embedded assets
 	mux.Handle("/static/*", http.FileServer(http.FS(assets)))
 
 	return mux
